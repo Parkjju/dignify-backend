8958c8338cfd973fcca8424b03b020f6952f350b
Parkjju <rudwns3927@gmail.com>
2026-06-26 16:11:42 -0700

    feat: add user profile and nickname update endpoints

--- src/main/java/com/rta/dignify/controller/UserController.java
+++ src/main/java/com/rta/dignify/controller/UserController.java
@@ -1,22 +1,35 @@
 package com.rta.dignify.controller;
 
 import com.rta.dignify.dto.hype.HypeListResponse;
+import com.rta.dignify.dto.user.NicknameUpdateRequest;
+import com.rta.dignify.dto.user.NicknameUpdateResponse;
+import com.rta.dignify.dto.user.UserProfileResponse;
 import com.rta.dignify.service.HypeService;
+import com.rta.dignify.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/users")
 @RestController
 public class UserController {
     private final HypeService hypeService;
+    private final UserService userService;
 
     @GetMapping("/me/hypes")
     public HypeListResponse getMyHypedTracks(@AuthenticationPrincipal Long userId, @RequestParam(required = false) Long cursor) {
         return hypeService.getMyHypedTracks(userId, cursor);
+    }
+
+    @GetMapping("/me")
+    public UserProfileResponse getUserProfile(@AuthenticationPrincipal Long userId) {
+        return userService.getUserProfile(userId);
+    }
+
+    @PatchMapping("/me/nickname")
+    public NicknameUpdateResponse changeUserNickname(@AuthenticationPrincipal Long userId, @RequestBody @Valid NicknameUpdateRequest request) {
+        return userService.changeUserNickname(userId, request);
     }
 }
