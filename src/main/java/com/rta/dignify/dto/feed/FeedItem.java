c6ed7cf74d0b0916bddcb0cb46423b794c6cd18c
Parkjju <rudwns3927@gmail.com>
2026-07-17 17:46:50 -0700

    feat: add genres to feeditem

--- src/main/java/com/rta/dignify/dto/feed/FeedItem.java
+++ src/main/java/com/rta/dignify/dto/feed/FeedItem.java
@@ -1,13 +1,16 @@
 package com.rta.dignify.dto.feed;
 
+import com.rta.dignify.domain.Genre;
 import com.rta.dignify.domain.Track;
 import org.springframework.context.i18n.LocaleContextHolder;
 
 import java.util.Locale;
 
-public record FeedItem(Long trackId, String trackName, String artistName, String artworkUrl, String previewUrl, String trackViewUrl, boolean isHyped) {
+public record FeedItem(Long trackId, String trackName, String artistName, String artworkUrl, String previewUrl, String trackViewUrl, String genreName, boolean isHyped) {
     public static FeedItem from(Track track, boolean isHyped) {
         Locale locale = LocaleContextHolder.getLocale();
-        return new FeedItem(track.getId(), track.displayTrackName(locale), track.displayArtistName(locale), track.getArtworkUrl(), track.getPreviewUrl(), track.displayTrackViewUrl(locale), isHyped);
+        Genre genre = track.getGenre();
+        String genreName = "ko".equals(locale.getLanguage()) ? genre.getGenreNameKo() : genre.getGenreNameEn();
+        return new FeedItem(track.getId(), track.displayTrackName(locale), track.displayArtistName(locale), track.getArtworkUrl(), track.getPreviewUrl(), track.displayTrackViewUrl(locale), genreName, isHyped);
     }
 }
